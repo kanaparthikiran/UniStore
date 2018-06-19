0d1a9c7e73c87bd0dd4c87c432f51ae97b49c4d0
Kiran Kanaparthi <kirank1@kirank1-mbr.telenav.com>
2018-06-18 22:39:55 -0700

    Added UniStore Initial Commit

---
+++ src/main/java/com/pitneybowes/application/UniStore/UniStoreApplication.java
@@ -0,0 +1,12 @@
+package com.pitneybowes.application.UniStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniStoreApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pitneybowes/application/UniStore/UniStoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pitneybowes.application.UniStore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UniStoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
