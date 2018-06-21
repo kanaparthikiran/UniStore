8181aad04ad7cf69cbb1b64e83df39f5949a5e34
Kiran Kanaparthi <kirank1@kirank1-mbr.telenav.com>
2018-06-21 11:07:07 -0700

    Updated Authentication Client Code

--- src/main/java/com/pitneybowes/AddressVerificationServiceController.java
+++ src/main/java/com/pitneybowes/AddressVerificationServiceController.java
@@ -3,10 +3,37 @@
  */
 package com.pitneybowes;
 
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
 /**
  * @author Kiran Kanaparthi
  *
  */
+@Controller
+@RequestMapping("/addressVerification/v1")
 public class AddressVerificationServiceController {
+
+    private static final Logger log = LoggerFactory.getLogger(AddressVerificationServiceController.class);
+
+    @Autowired
+    AuthenticationUtil authenticationUtil;
+    
+    
+    @RequestMapping("/verify")
+	public String welcome(Map<String, Object> model) {
+		
+      RestTemplate restTemplate = new RestTemplate();
+      String accessToken = authenticationUtil.getAccessToken();
+
+		return "welcome";
+	}
 
 }
